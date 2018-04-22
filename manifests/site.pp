8c40061d576438fb0d618ee263f3d6d94874b2fb
David Jeche GDS <david.jeche@digital.cabinet-office.gov.uk>
2018-04-22 22:36:24 +0100

    Removing test from repo & performing site apply

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,6 +5,10 @@ $defaults_users_settings = {
   managehome => true,
 }
 
+file { '/root/site-conf.test':
+    ensure => 'file',
+}
+
 cron { 'puppet_run_gitpull':
   command => 'cd /re-promtheus-cm && /usr/bin/git -p pull origin master && /opt/puppetlabs/bin/puppet apply /re-prometheus-cm/manifests/ --hiera_config=/re-prometheus-cm/hiera.yaml >> /var/log/puppet_run',
   user    => 'root',
