c1ed2044c38d7756fe016a9223e1a961dda0918e
David Jeche GDS <david.jeche@digital.cabinet-office.gov.uk>
2018-04-22 19:34:41 +0100

    Changes to be pushed to puppet run manifest

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,15 @@
 $user_accounts = lookup('accounts::user')
 $ssh_keys = lookup('accounts::sshkeys')
 
-$defaults_users = {
-  'managehome' => true,
+$defaults_users_settings = {
+  managehome => true,
 }
 
+cron { 'motherbase':
+  command => 'cd /motherbase && /usr/bin/git -p pull origin master',
+  user    => 'root',
+  ensure  => present,
+}
 
-create_resources(user, $user_accounts, $defaults_users)
+create_resources(user, $user_accounts, $defaults_users_settings)
 create_resources(ssh_authorized_key, $ssh_keys)
